213b9f7b7530f7ac838675ae4b2bdfd1b17ca4ba
Steve Goldthorpe <steve.goldthorpe@hornbill.com>
2017-10-25 14:38:52 +0100

    Report filename changes
    Added support for new report filename changes in Hornbill platform.

--- PowerBIDataSource_HistoricReport.R
+++ PowerBIDataSource_HistoricReport.R
@@ -9,12 +9,59 @@ apiKey = "API key goes here"
 reportID = "Report primary key (INT) goes here"
 reportRunID = "Run ID (INT) of historic report run goes here"
 
+# Build XMLMC URL
+arrUrl = c("https://", 
+           instanceZone, 
+           "api.hornbill.com/", 
+           instanceName)
+
+xmlmcURL = paste(arrUrl, collapse="")
+
 # Import dependencies
 library('RCurl')
 library('XML')
 
-# Build CSV filename from report ID and run ID 
-reportCSVLink = paste(reportID, "_", reportRunID, ".csv", sep="")
+# invokeXmlmc - take params, fire off XMLMC call
+invokeXmlmc = function(url, key, service, xmethod, params)
+{
+  # Build Methodcall
+  paramsrequest = paste(params , collapse="")
+  arrRequest = c(	"<methodCall service=\"",
+                  service, 
+                  "\" method=\"",
+                  xmethod,
+                  "\">",
+                  paramsrequest,
+                  "</methodCall>")
+  
+  request = paste(arrRequest, collapse="")
+  
+  # Build Invoke URL
+  invokeURL = paste(url, "/xmlmc/", service, "/?method=", xmethod, sep="")
+  
+  # Build Headers
+  espKeyAuth = paste('ESP-APIKEY ', key, sep="")
+  requestHeaders = c('Content-Type'='text/xmlmc',
+                     'Cache-control'='no-cache',
+                     'Accept'='text/xml',
+                     'Authorization'=espKeyAuth)
+  
+	data = getURL(	url = invokeURL,
+                  postfields=request,
+		      httpheader=requestHeaders,
+        		verbose=TRUE)
+	return(data)
+}
+
+# Get CSV filename from report ID and run ID 
+arrXmlmcRequest = c(	"<params>",
+                        "<runId>", reportRunID, "</runId>",
+                        "</params>")
+xml.request = paste(arrXmlmcRequest , collapse="")
+
+reportRunStatus = invokeXmlmc(xmlmcURL, apiKey, "reporting", "reportRunGetStatus", xml.request)
+xmlRunStatus  = xmlTreeParse(reportRunStatus, asText = TRUE,useInternalNodes=T)
+reportCSVLink = unlist(xpathApply(xmlRunStatus,'//methodCallResult/params/reportRun/csvLink',xmlValue))
 
 # Build XMLMC URL
 arrUrl = c("https://", 
